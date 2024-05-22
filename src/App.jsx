2d393504c18c88ffc22165e4be7058ddce82eb98
milena <milena-.dias@live.com>
2024-05-22 11:57:47 -0300

    contador

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [contador, setContador] = useState(0);
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
+
+  const decrementar = () => {
+    setContador(contador - 1);
+  };
+
+  return (
+    <div>
+      <h2>Contador</h2>
+      <p>Valor: {contador}</p>
+      <button onClick={incrementar}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
+    </div>
+  );
+}
+
+export default App
